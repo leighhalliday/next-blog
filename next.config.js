a052355438ed2f553ee292cf884c59a7288b09ed
Pearcekieser <5055971+Pearcekieser@users.noreply.github.com>
2022-04-23 01:32:26 -0400

    Include MDX files in the webpack watch
    Includes MDX files so if you are editing you blog post all you need to do is refresh the page for an updated view, no need to force the web app to rebuild.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    // https://stackoverflow.com/a/59954986
+    config.watchOptions.ignored = config.watchOptions.ignored.filter(
+      ignore => !ignore.toString().includes('.mdx')
+    );
+    return config;
+  }
 }
+
+module.exports = nextConfig
